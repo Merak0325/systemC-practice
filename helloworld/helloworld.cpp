373aa6579a4b6411b4c2051303d886b64acbc1c5
root <merak_official@yahoo.com>
2023-07-30 15:59:14 +0000

    progress: time notation

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -1,23 +1,20 @@
 // Learn with Examples, 2020, MIT license
-#include <systemc> // include the systemC header file
-using namespace sc_core; // use namespace
+#include <systemc>
+using namespace sc_core;
 
-void hello1() { // a normal c++ function
-  std::cout << "Hello world using approach 1" << std::endl;
-}
-
-struct HelloWorld : sc_module { // define a systemC module
-  SC_CTOR(HelloWorld) {// constructor function, to be explained later
-    SC_METHOD(hello2); // register a member function to the kernel
-  }
-  void hello2(void) { // a function for systemC simulation kernel, void inside () can be omitted
-    std::cout << "Hello world using approach 2" << std::endl;
-  }
-};
-
-int sc_main(int, char*[]) { // entry point
-  hello1(); // approach #1: manually invoke a normal function
-  HelloWorld helloworld("helloworld"); // approach #2, instantiate a systemC module
-  sc_start(); // let systemC simulation kernel to invoke helloworld.hello2();
+int sc_main(int, char*[]) {
+sc_core::sc_report_handler::set_actions( "/IEEE_Std_1666/deprecated",
+                                           sc_core::SC_DO_NOTHING ); // suppress warning due to set_time_resolution
+  sc_set_time_resolution(1, SC_FS); // deprecated function but still useful, default is 1 PS
+  sc_set_default_time_unit(1, SC_SEC); // change time unit to 1 second
+  std::cout << "1 SEC =     " << sc_time(1, SC_SEC).to_default_time_units() << " SEC"<< std::endl;
+  std::cout << "1  MS = " << sc_time(1, SC_MS).to_default_time_units()  << " SEC"<< std::endl;
+  std::cout << "1  US = " << sc_time(1, SC_US).to_default_time_units()  << " SEC"<< std::endl;
+  std::cout << "1  NS = " << sc_time(1, SC_NS).to_default_time_units()  << " SEC"<< std::endl;
+  std::cout << "1  PS = " << sc_time(1, SC_PS).to_default_time_units()  << " SEC"<< std::endl;
+  std::cout << "1  FS = " << sc_time(1, SC_FS).to_default_time_units()  << " SEC"<< std::endl;
+  sc_start(7261, SC_SEC); // run simulation for 7261 second
+  double t = sc_time_stamp().to_seconds(); // get time in second
+  std::cout << int(t) / 3600 << " hours, " << (int(t) % 3600) / 60 << " minutes, " << (int(t) % 60) << "seconds" << std::endl;
   return 0;
 }
